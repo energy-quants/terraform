56bcdb7b3da80975a8213dc213adedb0e198b2c3
Dave Hirschfeld <dave.hirschfeld@gmail.com>
2024-08-25 14:10:28 +0000

    Fix `vnet` outputs

--- src/vnet/outputs.tf
+++ src/vnet/outputs.tf
@@ -1,27 +1,27 @@
-variable "id" {
+output "id" {
   value = azurerm_virtual_network.vnet.id
 }
 
-variable "name" {
+output "name" {
    value = azurerm_virtual_network.vnet.name
 }
 
-variable "location" {
+output "location" {
    value = azurerm_virtual_network.vnet.location
 }
 
-variable "resource_group_name" {
+output "resource_group_name" {
    value = azurerm_virtual_network.vnet.resource_group_name
 }
 
-variable "address_space" {
+output "address_space" {
    value = azurerm_virtual_network.vnet.address_space
 }
 
-variable "guid" {
+output "guid" {
    value = azurerm_virtual_network.vnet.guid
 }
 
-variable "dns_servers" {
+output "dns_servers" {
    value = azurerm_virtual_network.vnet.dns_servers
 }
